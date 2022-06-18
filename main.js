561847358d54f96ebc364703fbf23e95c06fa341
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2022-06-18 14:40:13 +0300

    Add electron and example

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        enableRemoteModule: true,
+    },
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
